d6292718d4cef3ce48fbd86f3cfdbd9ac630354f
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-01-09 09:43:52 +0530

    FlightFinal

--- dummy.js
+++ dummy.js
@@ -1,4 +1,4 @@
-import { header,footer } from "./Header_Footer/header_footer.js";
+import { header,footer } from "./Header_Footer.js";
 
 document.getElementById("n").innerHTML=header();
 document.getElementById("f").innerHTML=footer();
